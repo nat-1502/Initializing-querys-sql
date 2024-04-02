71103ab10a698fadb82a419043281d9d9644db0f
nat-1502 <natalisoares90@outlook.com>
2024-04-02 09:06:34 -0300

    where

---
+++ Initializing-querys-sql.sql
@@ -0,0 +1,20 @@
+
+
+SELECT Name, Weight
+FROM Production.Product
+WHERE Weight > 500 AND Weight <=700;
+
+
+
+SELECT *
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'M' AND SalariedFlag = '1';
+
+
+SELECT * 
+FROM person.Person 
+WHERE FirstName = 'Peter' AND LastName = 'Krebs';
+
+Select *
+FROM Person.EmailAddress 
+WHERE BusinessEntityID = '26';
